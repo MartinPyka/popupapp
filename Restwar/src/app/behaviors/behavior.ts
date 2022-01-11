e37f333081f6264d29a1c280cbcbe70c0557380b
Martin Pyka <martin.pyka@gmail.com>
2022-01-11 23:30:37 +0100

    added callbacks for behavior

--- Restwar/src/app/behaviors/behavior.ts
+++ Restwar/src/app/behaviors/behavior.ts
@@ -1,9 +1,18 @@
 import { Mechanism } from '../model/mechanisms/mechanism';
 import { IDisposable } from '@babylonjs/core';
+import { Subscription } from 'rxjs';
+import { IModelDisposable } from '../model/interfaces/interfaces';
 
 /** abstract and generic class for all kinds of behaviors */
-export abstract class Behavior implements IDisposable {
-  constructor(mechanism: Mechanism) {}
+export abstract class Behavior implements IModelDisposable {
+  readonly subscriptionList: Subscription[];
 
-  dispose(): void {}
+  constructor(mechanism: Mechanism) {
+    this.subscriptionList = [];
+  }
+
+  dispose(): void {
+    this.subscriptionList.forEach((subscription) => subscription.unsubscribe());
+    this.subscriptionList.length = 0;
+  }
 }
--- Restwar/src/app/behaviors/mechanism/Behavior.BookletControl.ts
+++ Restwar/src/app/behaviors/mechanism/Behavior.BookletControl.ts
@@ -3,19 +3,25 @@ import { Behavior } from '../behavior';
 import { AppInjector } from 'src/app/app.module';
 import { EditorService } from 'src/app/core/editor-service';
 import { PlaneRectangle } from 'src/app/model/planes/plane.rectangle';
-import { Vector3 } from '@babylonjs/core';
+import { Scene, Vector3 } from '@babylonjs/core';
 import { MaterialService } from 'src/app/materials/material-service';
 import { Channel } from 'src/app/core/emitter.channels';
 import { IDisposable } from '@babylonjs/core';
+import { first } from 'rxjs';
 
 const HANDLE_WIDTH = 3;
 const HANDLE_HEIGHT = 3;
 
 export class BehaviorBookletControl extends Behavior implements IDisposable {
+  // mechanism this behavior is operating on
   protected mechanism: MechanismActive;
 
   protected leftHandle: PlaneRectangle;
   protected rightHandle: PlaneRectangle;
+
+  // temporary storage of current value for CommandInvoker
+  private leftAngle: number = 0;
+  private rightAngle: number = 0;
 
   constructor(mechanism: MechanismActive) {
     super(mechanism);
@@ -23,6 +29,11 @@ export class BehaviorBookletControl extends Behavior implements IDisposable {
     const editorService = AppInjector.get(EditorService);
     const scene = editorService.scene;
 
+    this.createGeometry(mechanism, scene);
+    this.registerEvents(editorService);
+  }
+
+  private createGeometry(mechanism: MechanismActive, scene: Scene): void {
     this.leftHandle = new PlaneRectangle(HANDLE_WIDTH, HANDLE_HEIGHT, scene, this.mechanism.leftSide);
     this.leftHandle.transform.position = new Vector3(
       this.mechanism.width.getValue() / 2 + HANDLE_WIDTH / 2,
@@ -38,8 +49,18 @@ export class BehaviorBookletControl extends Behavior implements IDisposable {
       0
     );
     this.rightHandle.material = MaterialService.matBookletHandle;
+  }
 
+  private registerEvents(editorService: EditorService) {
+    // list on Booklet_Handle-Events
     editorService.onSelectionMode.on(Channel.BOOKLET_HANDLE, (value: boolean) => {});
+
+    this.subscriptionList.push(
+      this.leftHandle.onPickDown.subscribe((planeClick) => {
+        this.leftAngle = this.mechanism.leftAngle.getValue();
+        this.rightAngle = this.mechanism.rightAngle.getValue();
+      })
+    );
   }
 
   public override dispose(): void {
